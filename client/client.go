88e700d930b42c68ce209ef8b5447ce7f877b85d
Manu730 <Manu730@users.noreply.github.com>
2024-07-22 01:29:38 +0530

    client final touchup

--- client/client.go
+++ client/client.go
@@ -11,27 +11,79 @@ import (
 
 func main() {
 	conn, err := grpc.Dial("localhost:8080", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		fmt.Println("\n\n Error connecting to server: ", err)
+		panic(err)
+	}
 	client := pb.NewTicketMasterClient(conn)
-
+	ctx := context.Background()
 	traveller := &pb.User{FirstName: "Manoj", LastName: "Kumar", Email: "manunero1730@gmail.com"}
 	payment := &pb.Price{Price: 20, Currency: "Dollar"}
-	bookTicketInput := &pb.Receipt{TrainNumber: 1, FromStation: "London", ToStation: "France", User: traveller, Cost: payment}
 
+	//Book ticket for a given user
+	bookTicketInput := &pb.Receipt{TrainNumber: 1, FromStation: "London", ToStation: "France", User: traveller, Cost: payment}
 	bookingresp, er := client.BookTicket(context.Background(), bookTicketInput)
 	if er != nil {
-		fmt.Println("Error fetching data: ", er)
-	}
-	fmt.Println("Recevied booking resp: ", bookingresp)
+		fmt.Println("\n\n Error Booking ticket: ", er)
+		panic(er)
 
-	allocationdata, err := client.ShowAllocations(context.Background(), &pb.ShowAllocationInput{TrainNumber: 1, Order: 0})
-	if err != nil {
-		fmt.Println("Error fetching data: ", err)
 	}
-	fmt.Println("Recevied data: ", allocationdata)
+	fmt.Println("\n\n Recevied booking response: ", bookingresp)
 
-	receipt, e := client.GetReceipt(context.Background(), &pb.UserTrainInput{TrainNumber: 1, User: traveller})
+	//Get receipt for the booked user
+	receipt, e := client.GetReceipt(ctx, &pb.UserTrainInput{TrainNumber: 1, User: traveller})
 	if e != nil {
-		fmt.Println("Error fetching data: ", e)
+		fmt.Println("\n\n Error fetching data: ", e)
 	}
-	fmt.Println("Recevied receipt: ", receipt)
+	fmt.Println("\n\n Recevied receipt: ", receipt)
+
+	//Show train allocation post ticket booking for section A
+	sectionAdata, err1 := client.ShowAllocations(ctx, &pb.ShowAllocationInput{TrainNumber: 1, Order: 0})
+	if err1 != nil {
+		fmt.Println("\n\n Error fetching allocation data: ", err1)
+	} else {
+		fmt.Println("\n\n Recevied allocation data for section A: ", sectionAdata)
+	}
+
+	//Show train allocation post ticket booking for section B
+	sectionBdata, err2 := client.ShowAllocations(ctx, &pb.ShowAllocationInput{TrainNumber: 1, Order: 1})
+	if err2 != nil {
+		fmt.Println("\n\n Error fetching allocation data: ", err2)
+	} else {
+		fmt.Println("\n\n Recevied allocation data for section B: ", sectionBdata)
+	}
+
+	//Remove User from train
+	_, err3 := client.RemoveUser(ctx, &pb.UserTrainInput{TrainNumber: 1, User: &pb.User{FirstName: "Manoj", LastName: "Kumar", Email: "manunero1730@gmail.com"}})
+	if err3 != nil {
+		fmt.Println("\n\n Error removing user from train: ", err3)
+	} else {
+		fmt.Println("\n\n User successfully removed from train")
+	}
+
+	//Book ticket again for user
+	client.BookTicket(context.Background(), bookTicketInput)
+	_, err4 := client.ModifyUserAllocation(ctx, &pb.UserAllocModifyInput{UserTrainDetails: &pb.UserTrainInput{TrainNumber: 1, User: &pb.User{FirstName: "Manoj", LastName: "Kumar", Email: "manunero1730@gmail.com"}}, NewSection: 1, NewSeatno: 10})
+	if err4 != nil {
+		fmt.Println("\n\n Error updating user allocation: ", err4)
+	} else {
+		fmt.Println("\n\n User allocation modified successfully")
+	}
+
+	//Show train allocation post user modification for section A
+	sectionAdata, err1 = client.ShowAllocations(ctx, &pb.ShowAllocationInput{TrainNumber: 1, Order: 0})
+	if err1 != nil {
+		fmt.Println("\n\n Error fetching allocation data: ", err1)
+	} else {
+		fmt.Println("\n\n Recevied allocation data for section A: ", sectionAdata)
+	}
+
+	//Show train allocation post user modification for section B
+	sectionBdata, err2 = client.ShowAllocations(ctx, &pb.ShowAllocationInput{TrainNumber: 1, Order: 1})
+	if err2 != nil {
+		fmt.Println("\n\n Error fetching allocation data: ", err2)
+	} else {
+		fmt.Println("\n\n Recevied allocation data for section B: ", sectionBdata)
+	}
+
 }
